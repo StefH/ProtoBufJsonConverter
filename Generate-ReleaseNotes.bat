de10ae5c1e8b373e7a6d0d61b21da8f124728086
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-09-23 20:03:49 +0200

    env

--- Generate-ReleaseNotes.bat
+++ Generate-ReleaseNotes.bat
@@ -2,6 +2,6 @@ rem https://github.com/StefH/GitHubReleaseNotes
 
 SET version=0.4.0
 
-C:\Dev\GitHub\GitHubReleaseNotes\src\GitHubReleaseNotes\bin\Debug\net8.0\win-x64\GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid documentation --version %version% --token %GH_TOKEN%
+C:\Dev\GitHub\GitHubReleaseNotes\src\GitHubReleaseNotes\bin\Debug\net8.0\win-x64\GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels env question invalid documentation --version %version% --token %GH_TOKEN%
 
-C:\Dev\GitHub\GitHubReleaseNotes\src\GitHubReleaseNotes\bin\Debug\net8.0\win-x64\GitHubReleaseNotes --output PackageReleaseNotes.txt --skip-empty-releases --exclude-labels question invalid documentation --template PackageReleaseNotes.template --version %version% --token %GH_TOKEN%
+C:\Dev\GitHub\GitHubReleaseNotes\src\GitHubReleaseNotes\bin\Debug\net8.0\win-x64\GitHubReleaseNotes --output PackageReleaseNotes.txt --skip-empty-releases --exclude-labels env question invalid documentation --template PackageReleaseNotes.template --version %version% --token %GH_TOKEN%
